377b35413ff33f31682c12cf2868e9d662805975
LeeJyeon <jihyun6969@nate.com>
2024-09-29 17:22:32 +0900

    rename

---
+++ src/main/kotlin/com/playground/client/body/FakeResponse.kt
@@ -0,0 +1,8 @@
+package com.playground.client.body
+
+import lombok.NoArgsConstructor
+
+data class FakeResponse(
+    val code:String = "",
+    val message:String = ""
+)
--- src/main/kotlin/com/playground/client/body/Response.kt
+++
@@ -1,6 +0,0 @@
-package com.playground.client.body
-
-data class Response(
-    val code:String,
-    val message:String
-)
